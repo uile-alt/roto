e5c9f7f1e1061d045adac156e92425ad9483b3b0
uile-alt <haotian908@gmail.com>
2021-01-09 15:10:41 +0800

    add icon and iconPosition

---
+++ src/components/button.vue
@@ -0,0 +1,64 @@
+<template>
+    <button class="r-button" :class="{[`icon-${iconPosition}`] : true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
+</template>
+<script>
+    export default {
+        props: ['icon', 'iconPosition'],
+    }
+</script>
+<style lang="scss">
+    .r-button {
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        vertical-align: middle;
+
+        > .icon {
+            order: 1;
+            margin-right: .1em;
+        }
+
+        > .content {
+            order: 2;
+        }
+
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+
+        &:focus {
+            outline: none;
+        }
+
+        &.icon-right {
+            > .icon {
+                order: 2;
+                margin-right: 0;
+                margin-left: .1em;
+
+            }
+
+            > .content {
+                order: 1;
+            }
+        }
+    }
+
+</style>
