25495f453d23e9b3e8140d5e1d5d8c233ff55e36
uile-alt <haotian908@gmail.com>
2021-01-10 17:53:06 +0800

    使用chai-spies 监听回调函数

--- src/app.js
+++ src/app.js
@@ -20,6 +20,8 @@ new Vue({
 })
 
 import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
 
 const expect = chai.expect;
 //单元测试
@@ -94,9 +96,9 @@ const expect = chai.expect;
         }
     })
     vm.$mount()
-    vm.$on('click', function () {
-        console.log(1)
-    })
+    let spy = chai.spy(function(){})
+    vm.$on('click', spy)
     let button = vm.$el
     button.click()
+    expect(spy).to.have.been.called()
 }
