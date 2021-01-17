8e1e36ee95890fcc39b81c90c41d83e5b09cabe3
uile-alt <haotian908@gmail.com>
2021-01-17 21:48:06 +0800

    input 支持v-model双向绑定

--- src/app.js
+++ src/app.js
@@ -1,26 +1,27 @@
 import Vue from 'vue'
 import Button from "./components/button"
-import Icon from  './components/icon'
+import Icon from './components/icon'
 import ButtonGroup from './components/button-group'
 import Input from './components/input'
 
 Vue.component('r-button', Button)
 Vue.component('r-icon', Icon)
-Vue.component('r-button-group',ButtonGroup)
-Vue.component('r-input',Input)
+Vue.component('r-button-group', ButtonGroup)
+Vue.component('r-input', Input)
 
 
 new Vue({
-    el: '#app',
-    data(){
-        return{
-            loading1: false,loading2: false,loading3: false,
-        }
-    },
-    methods:{
-      inputChange(e){
-        console.log(e.target.value)
-      }
+  el: '#app',
+  data() {
+    return {
+      loading1: false, loading2: false, loading3: false,
+      message: 0
     }
+  },
+  methods: {
+    inputChange(e) {
+      console.log(e.target.value)
+    }
+  }
 })
 
