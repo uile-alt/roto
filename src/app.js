572cf65adee08a0d6dede6a66ad903f88320f799
uile-alt <haotian908@gmail.com>
2021-01-20 21:18:02 +0800

    初步row和col

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from "./components/button"
 import Icon from './components/icon'
 import ButtonGroup from './components/button-group'
 import Input from './components/input'
+import Row from './components/row'
+import Col from './components/col'
 
 Vue.component('r-button', Button)
 Vue.component('r-icon', Icon)
 Vue.component('r-button-group', ButtonGroup)
 Vue.component('r-input', Input)
+Vue.component('r-col', Col)
+Vue.component('r-row', Row)
 
 
 new Vue({
---
+++ src/components/col.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="col" :class="[`col_${span}`]">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name:'RotoCol',
+    props:{
+      span: {
+        type:[Number,String]
+      }
+    }
+}
+</script>
+<style lang="scss" scoped>
+    .col{
+        height: 100px;
+        background: beige;
+        width: 50%;
+        border: 1px solid red;
+
+        $class: col_;
+        @for $n from 1 through 24 {
+            &.#{$class}#{$n} {
+                width: ($n / 24) * 100%;
+            }
+        }
+    }
+</style>
