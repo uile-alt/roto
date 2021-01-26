d1964c605add45c95a77307e7ff5887e0ea16ae3
uile-alt <haotian908@gmail.com>
2021-01-26 22:24:04 +0800

    完成基本布局

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import ButtonGroup from './components/button-group'
 import Input from './components/input'
 import Row from './components/row'
 import Col from './components/col'
+import Content from './components/content'
+import Footer from './components/footer'
+import Sider from './components/sider'
+import Layout from './components/layout'
+import Header from './components/header'
 
 Vue.component('r-button', Button)
 Vue.component('r-icon', Icon)
@@ -12,6 +17,11 @@ Vue.component('r-button-group', ButtonGroup)
 Vue.component('r-input', Input)
 Vue.component('r-col', Col)
 Vue.component('r-row', Row)
+Vue.component('r-content', Content)
+Vue.component('r-footer', Footer)
+Vue.component('r-sider', Sider)
+Vue.component('r-layout', Layout)
+Vue.component('r-header', Header)
 
 
 new Vue({
---
+++ src/components/layout.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="layout" :class="layoutClass">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+      name: 'RotoLayout',
+      data(){
+        return{
+          layoutClass:{
+            hasSider:false
+          }
+        }
+      },
+      mounted(){
+        this.$children.forEach((vm)=>{
+          if(vm.$options.name === 'RotoSider'){
+            this.layoutClass.hasSider = true
+          }
+        })
+      }
+    }
+
+</script>
+<style lang="scss" scoped>
+    .layout{
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column;
+        border: 1px solid black;
+        &.hasSider{
+            flex-direction: row;
+        }
+    }
+</style>
