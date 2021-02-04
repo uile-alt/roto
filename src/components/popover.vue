8aaa7db5f43afe6315a23c70c4d5248c66e00fee
uile-alt <haotian908@gmail.com>
2021-02-04 20:18:58 +0800

    解决当容器有 overflow hidden 造成的bug

--- src/components/popover.vue
+++ src/components/popover.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="popover" @click.stop="change">
-        <div class="content-wrapper" v-if="visible" @click.stop>
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
             <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
+
     </div>
 </template>
 <script>
@@ -19,6 +22,11 @@
         this.visible = !this.visible
         if (this.visible === true) {
           this.$nextTick(() => {
+            let {left, width, top, height} = this.$refs.triggerWrapper.getBoundingClientRect()
+            console.log(left,top,width,height)
+            this.$refs.contentWrapper.style.left = left + window.scrollX +'px'
+            this.$refs.contentWrapper.style.top = top + window.scrollY +'px'
+            document.body.appendChild(this.$refs.contentWrapper)
             let eventHandler = () => {
               this.visible = false
               document.removeEventListener('click', eventHandler)
@@ -27,6 +35,8 @@
           })
         }
       }
+    },
+    mounted() {
     }
   }
 </script>
@@ -36,14 +46,11 @@
         vertical-align: top;
         border: 1px solid red;
         position: relative;
-
-        .content-wrapper {
-            position: absolute;
-            bottom: 100%;
-            left: 0;
-            border: 1px solid green;
-            box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-        }
     }
-
+    .content-wrapper {
+        position: absolute;
+        border: 1px solid green;
+        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+        transform: translateY(-100%);
+    }
 </style>
